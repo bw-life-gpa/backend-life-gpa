e9d7da2c0ff0492fd946f7532b8c5afbb0b1d5c9
siratl <elishubax@gmail.com>
2019-03-14 13:13:46 -0700

    Initialized tests: Register and Login tests written and passed

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -1,8 +1,11 @@
 const request = require('supertest');
 
-const server = require('../api/server');
+const server = require('../api/server.js');
 
-describe('Server.js', () => {
+const db = require('../database/dbConfig.js');
+//const users = await db('users');
+
+describe('Server Routes', () => {
   it('should set testing environment', () => {
     expect(process.env.DB_ENV).toBe('testing');
   });
@@ -11,6 +14,39 @@ describe('Server.js', () => {
     it('should return 200 OK', async () => {
       const res = await request(server).get('/');
       expect(res.status).toBe(200);
+    });
+
+    it('should return instruction to access endpoints in HTML TEXT', async () => {
+      const res = await request(server).get('/');
+      expect(res.type).toBe('text/html');
+    });
+  });
+
+  describe('Register Endpoint', () => {
+    it('should insert a user into the db and return 201', async () => {
+      const res = await request(server)
+        .post('/api/register')
+        .send({
+          username: 'siratl',
+          password: '1234',
+          fullName: 'Elisha Atulomah',
+          email: 'eli@gmail.com',
+          userImgUrl: 'animage.jpg',
+        });
+
+      expect(res.status).toBe(201);
+    });
+
+    describe('Login Endpoint', () => {
+      afterEach(async () => {
+        await db('users').truncate();
+      });
+      it('should return status 200 and provide token', async () => {
+        const res = await request(server)
+          .post('/api/login')
+          .send({ username: 'siratl', password: '1234' });
+        expect(res.status).toBe(200);
+      });
     });
   });
 });
